0a283209c654e5401080beeb01e697e5907f202f
Joaquin I. Corradi <corradijoaquin@gmail.com>
2022-11-03 18:55:37 -0300

    Iniciando proyecto

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
+
+struct competidor
+{
+  int numeroDeInscripcion;
+  std::string nombre;
+  int categoria, subcategoria;
+  int tirosCentro, tirosNoCentro;
+};
+
+void menu();
 
 int main()
 {
-  
+  int opcion = 1;
+  std::ifstream
+}
+
+void menu(int &opcion)
+{
+  while (opcion <= 0 || opcion > 6)
+  {
+    std::cout << "\tMENU PRINCIPAL:" << '\n';
+    std::cout << "\t\t1. Competidores inscriptos." << '\n';
+    std::cout << "\t\t2. Mostrar puntaje de competidores." << '\n';
+    std::cout << "\t\t3. Exportar puntajes (.txt)"
+    std::cout << "\t\t4. Filtrar competidores por un puntaje maximo." << '\n';
+    std::cout << "\t\t5. Mostrar cantidad de competidores por categoria y subcategoria." << '\n';
+    std::cout << "\t\t6. Salir." << '\n';
+    std::cout << "\tSeleccione una opcion (1 - 6): " << '\n';
+    std::cin << opcion;
+  }
 }
