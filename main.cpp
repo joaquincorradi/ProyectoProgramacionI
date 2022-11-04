42789ace9639f58cc954b5d1285b47e741f3354b
Joaquin I. Corradi <corradijoaquin@gmail.com>
2022-11-03 23:36:34 -0300

    Chequenado funciones

--- main.cpp
+++ main.cpp
@@ -10,11 +10,14 @@ struct competidor
 };
 
 void menu(int &opcion);
+void opcion1(std::ifstream &inscriptos);
+void opcion2();
 
 int main()
 {
   int opcion = 1;
-  std::ifstream insciptos("inscriptos.txt");
+  std::ifstream inscriptos("inscriptos.txt");
+  opcion1(inscriptos);
 }
 
 void menu(int &opcion)
@@ -22,7 +25,7 @@ void menu(int &opcion)
   while (opcion <= 0 || opcion > 6)
   {
     std::cout << "\tMENU PRINCIPAL:" << '\n';
-    std::cout << "\t\t1. Competidores inscriptos." << '\n';
+    std::cout << "\t\t1. Mostrar competidores inscriptos." << '\n';
     std::cout << "\t\t2. Mostrar puntaje de competidores." << '\n';
     std::cout << "\t\t3. Exportar puntajes (.txt)" << '\n';
     std::cout << "\t\t4. Filtrar competidores por un puntaje maximo." << '\n';
@@ -30,5 +33,15 @@ void menu(int &opcion)
     std::cout << "\t\t6. Salir." << '\n';
     std::cout << "\tSeleccione una opcion (1 - 6): " << '\n';
     std::cin >> opcion;
+  }
+}
+
+void opcion1(std::ifstream &inscriptos) // no esta imprimiendo los nombres!!!!
+{
+  int tempi;
+  std::string temps;
+  while (inscriptos >> tempi >>  temps)
+  {
+    std::cout << temps << '\n';
   }
 }
