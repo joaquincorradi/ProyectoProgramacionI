815e07fa964f2ea398eb77398c0de2c53992d983
Joaquin I. Corradi <corradijoaquin@gmail.com>
2022-11-04 20:16:26 -0300

    Reordenamiento de funciones

--- main.cpp
+++ main.cpp
@@ -14,32 +14,18 @@ struct competidor
   int tirosCentro, tirosNoCentro;
 };
 
-void menu(int &opcion);
 void asignar(competidor datos[20]);
+void menu(int &opcion);
 void opcion1(competidor datos[20]);
 void opcion2(competidor datos[20], int puntajes[20]);
 void opcion4(competidor datos[20], int puntajes[20]);
 
 int main()
 {
-  int opcion = 1;
   competidor datos[20];
+  int opcion = 1;
   asignar(datos);
   int puntajes[20];
-  opcion4(datos, puntajes);
-}
-
-void menu(int &opcion)
-{
-  std::cout << "\tMENU PRINCIPAL:" << '\n';
-  std::cout << "\t\t1. Mostrar competidores inscriptos." << '\n'; //terminada y chequeada
-  std::cout << "\t\t2. Mostrar puntaje de competidores." << '\n'; //terminada y chequeada
-  std::cout << "\t\t3. Exportar puntajes (.txt)" << '\n'; //falta hacer
-  std::cout << "\t\t4. Filtrar competidores por un puntaje maximo." << '\n'; //terminado y chequeado, no a prueba de usuario tonto
-  std::cout << "\t\t5. Mostrar cantidad de competidores por categoria y subcategoria." << '\n'; //falta hacer
-  std::cout << "\t\t6. Salir." << '\n';
-  std::cout << "\tSeleccione una opcion (1 - 6): " << '\n';
-  std::cin >> opcion; 
 }
 
 void asignar(competidor datos[20])
@@ -60,8 +46,20 @@ void asignar(competidor datos[20])
     inscriptos >> datos[i].tirosNoCentro;
   }
   inscriptos.close();
- }
+}
 
+void menu(int &opcion)
+{
+  std::cout << "\tMENU PRINCIPAL:" << '\n';
+  std::cout << "\t\t1. Mostrar competidores inscriptos." << '\n'; //terminada y chequeada
+  std::cout << "\t\t2. Mostrar puntaje de competidores." << '\n'; //terminada y chequeada
+  std::cout << "\t\t3. Exportar puntajes (.txt)" << '\n'; //falta hacer
+  std::cout << "\t\t4. Filtrar competidores por un puntaje maximo." << '\n'; //terminado y chequeado, no a prueba de usuario tonto
+  std::cout << "\t\t5. Mostrar cantidad de competidores por categoria y subcategoria." << '\n'; //falta hacer
+  std::cout << "\t\t6. Salir." << '\n';
+  std::cout << "\tSeleccione una opcion (1 - 6): " << '\n';
+  std::cin >> opcion; 
+}
 
 void opcion1(competidor datos[20])
 {
