63f0f3a5228a83b4ecacd30f28a890a2673bc493
corradijoaquin(elementaryOS) <corradijoaquin@gmail.com>
2022-11-06 00:53:29 -0300

    funcion opcion 5 terminada, es increible lo que se me da

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ struct competidor
 };
 
 void asignar(competidor datos[20]); //terminada y chequeada
-void menu(int &opcion); //terminada, falta chequear
+void menu(int &opcion); //terminada y chequeada
 void opcion1(competidor datos[20]); //terminada y chequeada
 void opcion2(competidor datos[20], int puntajes[20]); //terminada y chequeada
 void opcion3(); //sin empezar
-void opcion4(competidor datos[20], int puntajes[20]); //termidada, falta chequear
-void opcion5(competidor datos[20], int categoriasMatrix[2][2]); //en proceso...
+void opcion4(competidor datos[20], int puntajes[20]); //terminada y chequeada
+void opcion5(competidor datos[20], int categoriasMatrix[3][3]); //terminada, faltan detalles de formato en la impresion de la matriz
 void inicializarMenu(); //sin empezar
 
 int main()
@@ -30,10 +30,8 @@ int main()
   int opcion = 1;
   asignar(datos);
   int puntajes[20];
-  int categoriasMatrix[2][2];
-  //opcion5(datos, categoriasMatrix);
-  opcion2(datos, puntajes);
-  opcion4(datos, puntajes);
+  int categoriasMatrix[3][3];
+  opcion5(datos, categoriasMatrix);
 }
 
 void asignar(competidor datos[20])
@@ -63,14 +61,15 @@ void asignar(competidor datos[20])
 
 void menu(int &opcion)
 {
-  std::cout << "\tMENU PRINCIPAL:" << '\n';
-  std::cout << "\t\t1. Mostrar competidores inscriptos." << '\n';
-  std::cout << "\t\t2. Mostrar puntaje de competidores." << '\n';
-  std::cout << "\t\t3. Exportar puntajes (.txt)" << '\n';
-  std::cout << "\t\t4. Filtrar competidores por un puntaje maximo." << '\n';
-  std::cout << "\t\t5. Mostrar cantidad de competidores por categoria y subcategoria." << '\n';
-  std::cout << "\t\t6. Salir." << '\n';
-  std::cout << "\tSeleccione una opcion [1 - 6]: " << '\n';
+  std::cout << '\n';
+  std::cout << "MENU PRINCIPAL:" << '\n';
+  std::cout << "\t1. Mostrar competidores inscriptos." << '\n';
+  std::cout << "\t2. Mostrar puntaje de competidores." << '\n';
+  std::cout << "\t3. Exportar puntajes (.txt)" << '\n';
+  std::cout << "\t4. Filtrar competidores por un puntaje maximo." << '\n';
+  std::cout << "\t5. Mostrar cantidad de competidores por categoria y subcategoria." << '\n';
+  std::cout << "\t6. Salir." << '\n';
+  std::cout << "\tSeleccione una opcion [1 - 6]: ";
   std::cin >> opcion; 
 }
 
@@ -104,7 +103,7 @@ void opcion4(competidor datos[20], int puntajes[20])
 {
   int suma;
   int puntaje;
-  int mayor;
+  int mayor = puntajes[0]; //es necesario asignarle el primer valor del array a esta variable para que funcione pero no se por que
   for (int i = 0; i < 20; ++i)
   {
     suma = (datos[i].tirosCentro * 10) + (datos[i].tirosNoCentro * 5);
@@ -125,6 +124,10 @@ void opcion4(competidor datos[20], int puntajes[20])
   {
     std::cout << "No hay competidores con puntaje mayor a " << puntaje << '\n';
   }
+  else if (puntaje < 0)
+  {
+    std::cout << "El valor ingresado no es valido." << '\n';
+  }
   else
   {
     std::cout << "Los competidores por encima de " << puntaje << " puntos son:" << '\n';
@@ -140,67 +143,71 @@ void opcion4(competidor datos[20], int puntajes[20])
   std::cout << '\n';
 }
 
-void opcion5(competidor datos[20], int categoriasMatrix[2][2])
+void opcion5(competidor datos[20], int categoriasMatrix[3][3])
 {
-  int tmp0[3], tmp1[3], tmp2[3]; //un array por cada categoria
-  for (int i = 0; i < 3; ++i) //estas son las 3 categorias
+  int tmp0[3] = {0, 0, 0};
+  int tmp1[3] = {0, 0, 0};
+  int tmp2[3] = {0, 0, 0}; //un array por cada categoria
+  for (int j = 0; j < 20; ++j) //estos son los 20 inscriptos
   {
-    for (int j = 0; j < 20; ++i) //estos son los 20 inscriptos
+    if (datos[j].categoria == 0)
     {
-      if (datos[j].categoria == 0)
+      if (datos[j].subcategoria == 0)
       {
-        for (int k = 0; k < 20; ++i)
-        {
-          if (datos[k].subcategoria == 0)
-          {
-            ++tmp0[0];
-          }
-          else if (datos[k].subcategoria == 1)
-          {
-            ++tmp0[1];
-          }
-          else if (datos[k].subcategoria == 2)
-          {
-            ++tmp0[2];
-          }
-        }
+        ++tmp0[0];
       }
-      else if (datos[j].categoria == 1)
+      else if (datos[j].subcategoria == 1)
       {
-        for (int l = 0; l < 20; ++i)
-        {
-          if (datos[l].subcategoria == 0)
-          {
-            ++tmp1[0];
-          }
-          else if (datos[l].subcategoria == 1)
-          {
-            ++tmp1[1];
-          }
-          else if (datos[l].subcategoria == 2)
-          {
-            ++tmp1[2];
-          }
-        }
+        ++tmp0[1];
       }
-      else if (datos[j].categoria == 2)
+      else if (datos[j].subcategoria == 2)
       {
-        for (int m = 0; m < 20; ++i)
-        {
-          if (datos[m].subcategoria == 0)
-          {
-            ++tmp2[0];
-          }
-          else if (datos[m].subcategoria == 1)
-          {
-            ++tmp2[1];
-          }
-          else if (datos[m].subcategoria == 2)
-          {
-            ++tmp2[2];
-          }
-        }
+        ++tmp0[2];
       }
     }
+    else if (datos[j].categoria == 1)
+    {
+      if (datos[j].subcategoria == 0)
+      {
+        ++tmp1[0];
+      }
+      else if (datos[j].subcategoria == 1)
+      {
+        ++tmp1[1];
+      }
+      else if (datos[j].subcategoria == 2)
+      {
+        ++tmp1[2];
+      }
+    }
+    else if (datos[j].categoria == 2)
+    {
+      if (datos[j].subcategoria == 0)
+      {
+        ++tmp2[0];
+      }
+      else if (datos[j].subcategoria == 1)
+      {
+        ++tmp2[1];
+      }
+      else if (datos[j].subcategoria == 2)
+      {
+        ++tmp2[2];
+      }
+    }
+  }
+  for (int n = 0; n < 3; ++n)
+  {
+    categoriasMatrix[0][n] = tmp0[n];
+    categoriasMatrix[1][n] = tmp1[n];
+    categoriasMatrix[2][n] = tmp2[n];
+  }
+  for (int o = 0; o < 3; ++o)
+  {
+    for (int p = 0; p < 3; ++p)
+    {
+      std::cout << " " << categoriasMatrix[o][p] << " ";
+    }
+    std::cout << '\n';
   }
 }
